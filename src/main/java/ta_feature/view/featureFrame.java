975485e95849ce0546620870c22bd78ead060987
KimGwangHoon12 <wwnr2qqkb2@naver.com>
2025-05-12 15:42:10 +0900

    addd

--- src/main/java/ta_feature/view/featureFrame.java
+++ src/main/java/ta_feature/view/featureFrame.java
@@ -178,7 +178,7 @@ public class featureFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-int selectedRow = getReservationTable().getSelectedRow();
+    int selectedRow = getReservationTable().getSelectedRow();
     controller.rejectSelectedReservation(selectedRow);
     }//GEN-LAST:event_jButton2ActionPerformed
 
