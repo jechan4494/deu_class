9c21fa65918f7b4ce780c84b6cfbc121abc67c81
KimGwangHoon12 <wwnr2qqkb2@naver.com>
2025-05-25 02:16:08 +0900

    [ㅁ]

--- src/main/java/shared/model/ta/Reservation.java
+++ src/main/java/shared/model/ta/Reservation.java
@@ -40,6 +40,10 @@ public class Reservation implements Serializable {
     public String getType() {
         return type;
     }
+    
+    public void setType(String type) {
+    this.type = type;
+    }
 
     public int getRoomNumber() {
         return roomNumber;
